e24df47ce31df739b69398a3769913373b694946
Yehor Tereshchenko <123106244+Yegmina@users.noreply.github.com>
2025-08-18 13:07:42 +0300

    Task2 Advent of code kinda Christmas tree :)

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,38 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome everyone! \n");
-        System.out.println("And another way to write it");
+        adventofcodetype(4);
 
-        System.out.print("Hello, World for the 31 time :)!!! \n");
-        FunComands.say("Just created this function and class to test Java language syntax");
+        System.out.println("Hey! If you re interested, enter the tree height: ");
+
+        Scanner scanner = new Scanner(System.in);
+        int numberLines = 0;
+
+
+        numberLines = scanner.nextInt(); //assuming user is writing int
+        scanner.close();
+
+
+        adventofcodetype(numberLines);
+
+    }
+    public static void adventofcodetype(int n) {
+        // n is number of lines in Christmas tree
+        String local_line="";
+
+        for (int i=0; i<n; i++){
+            local_line=" ".repeat(n-i) + "*".repeat(1+i*2);
+            FunComands.say(local_line);
+
+        }
+
     }
 
 }
+
+
