26a29e5685e0c1d37c6e73d43ad3643e19d2cb26
Yehor Tereshchenko <123106244+Yegmina@users.noreply.github.com>
2025-08-18 11:08:45 +0300

    Initial commit Hellow World for the 31-st time

---
+++ src/FunComands.java
@@ -0,0 +1,16 @@
+public class FunComands {
+    public static void say(String... local_lines) {
+        if  (local_lines.length == 0){
+            System.out.println("There is nothing to say. Error, kinda. The code was silent...");
+        }
+        else if  (local_lines.length == 1){
+            System.out.println(local_lines[0]);
+
+        }
+        else {
+            for (String localLine : local_lines) {
+                System.out.println(localLine);
+            }
+        }
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome everyone! \n");
+        System.out.println("And another way to write it");
+
+        System.out.print("Hello, World for the 31 time :)!!! \n");
+        FunComands.say("Just created this function and class to test Java language syntax");
+    }
+
+}
